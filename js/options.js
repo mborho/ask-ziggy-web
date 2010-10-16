68d0facd4aadfbfdc52633d7f163899d48a15d04
mborho <martin@borho.net>
2010-10-16 11:06:27 +0200

    jsonified data objects

--- js/options.js
+++ js/options.js
@@ -1,247 +1,60 @@
-var oLangs = Object();
-oLangs['dk'] = 'Dansk';
-oLangs['nl'] = 'Nederlands';
-oLangs['en'] = 'English';
-oLangs['fi'] = 'Suomi';
-oLangs['fr'] = 'Français';
-oLangs['de'] = 'Deutsch';
-oLangs['it'] = 'Italiano';
-oLangs['no'] = 'Norsk';
-oLangs['ro'] = 'Română';
-oLangs['ru'] = 'Русский';
-oLangs['es'] = 'Español';
-oLangs['tr'] = 'Türkçe';
-
-var oGtlate = Object();
-oGtlate['af'] = 'Afrikaans';
-oGtlate['sq'] = 'Albanian';
-oGtlate['ar'] = 'Arabic';
-oGtlate['be'] = 'Belarusian';
-oGtlate['bg'] = 'Bulgarian';
-oGtlate['ca'] = 'Catalan';
-oGtlate['zh-CN'] = 'Chinese (Simplified)';
-oGtlate['zh-TW'] = 'Chinese (Traditional)';
-oGtlate['hr'] = 'Croatian';
-oGtlate['cs'] = 'Czech';
-oGtlate['da'] = 'Danish';
-oGtlate['nl'] = 'Dutch';
-oGtlate['en'] = 'English';
-oGtlate['et'] = 'Estonian';
-oGtlate['tl'] = 'Filipino';
-oGtlate['fi'] = 'Finnish';
-oGtlate['fr'] = 'French';
-oGtlate['gl'] = 'Galician';
-oGtlate['de'] = 'German';
-oGtlate['el'] = 'Greek';
-oGtlate['ht'] = 'Haitian Creole';
-oGtlate['iw'] = 'Hebrew';
-oGtlate['hi'] = 'Hindi';
-oGtlate['hu'] = 'Hungarian';
-oGtlate['is'] = 'Icelandic';
-oGtlate['id'] = 'Indonesian';
-oGtlate['ga'] = 'Irish';
-oGtlate['it'] = 'Italian';
-oGtlate['ja'] = 'Japanese';
-oGtlate['lv'] = 'Latvian';
-oGtlate['lt'] = 'Lithuanian';
-oGtlate['mk'] = 'Macedonian';
-oGtlate['ko'] = 'Korean';
-oGtlate['ms'] = 'Malay';
-oGtlate['mt'] = 'Maltese';
-oGtlate['no'] = 'Norwegian';
-oGtlate['fa'] = 'Persian';
-oGtlate['pl'] = 'Polish';
-oGtlate['pt'] = 'Portuguese';
-oGtlate['ro'] = 'Romanian';
-oGtlate['ru'] = 'Russian';
-oGtlate['sr'] = 'Serbian';
-oGtlate['sk'] = 'Slovak';
-oGtlate['sl'] = 'Slovenian';
-oGtlate['es'] = 'Spanish';
-oGtlate['sw'] = 'Swahili';
-oGtlate['sv'] = 'Swedish';
-oGtlate['th'] = 'Thai';
-oGtlate['tr'] = 'Turkish';
-oGtlate['uk'] = 'Ukrainian';
-oGtlate['vi'] = 'Vietnamese';
-oGtlate['cy'] = 'Welsh';
-oGtlate['yi'] = 'Yiddish';
-
-var oGlangs = Object();
-oGlangs['lang_af'] = 'Afrikaans';
-oGlangs['lang_ar'] = 'Arabic';
-oGlangs['lang_be'] = 'Belarusian';
-oGlangs['lang_bg'] = 'Bulgarian';
-oGlangs['lang_ca'] = 'Catalan';
-oGlangs['lang_zh-CN'] = 'Chinese (Simplified)';
-oGlangs['lang_zh-TW'] = 'Chinese (Traditional)';
-oGlangs['lang_hr'] = 'Croatian';
-oGlangs['lang_cs'] = 'Czech';
-oGlangs['lang_da'] = 'Danish';
-oGlangs['lang_nl'] = 'Dutch';
-oGlangs['lang_en'] = 'English';
-oGlangs['lang_eo'] = 'Esperanto';
-oGlangs['lang_et'] = 'Estonian';
-oGlangs['lang_tl'] = 'Filipino';
-oGlangs['lang_fi'] = 'Finnish';
-oGlangs['lang_fr'] = 'French';
-oGlangs['lang_de'] = 'German';
-oGlangs['lang_el'] = 'Greek';
-oGlangs['lang_iw'] = 'Hebrew';
-oGlangs['lang_hu'] = 'Hungarian';
-oGlangs['lang_is'] = 'Icelandic';
-oGlangs['lang_id'] = 'Indonesian';
-oGlangs['lang_it'] = 'Italian';
-oGlangs['lang_ja'] = 'Japanese';
-oGlangs['lang_ko'] = 'Korean';
-oGlangs['lang_lv'] = 'Latvian';
-oGlangs['lang_lt'] = 'Lithuanian';
-oGlangs['lang_no'] = 'Norwegian';
-oGlangs['lang_fa'] = 'Persian';
-oGlangs['lang_pl'] = 'Polish';
-oGlangs['lang_pt'] = 'Portuguese';
-oGlangs['lang_ro'] = 'Romanian';
-oGlangs['lang_ru'] = 'Russian';
-oGlangs['lang_sr'] = 'Serbian';
-oGlangs['lang_sk'] = 'Slovak';
-oGlangs['lang_sl'] = 'Slovenian';
-oGlangs['lang_es'] = 'Spanish';
-oGlangs['lang_sw'] = 'Swahili';
-oGlangs['lang_sv'] = 'Swedish';
-oGlangs['lang_th'] = 'Thai';
-oGlangs['lang_tr'] = 'Turkish';
-oGlangs['lang_uk'] = 'Ukrainian';
-oGlangs['lang_vi'] = 'Vietnamese';
-
-var oGeditions = Object();
-oGeditions['us'] = 'U.S.';
-oGeditions['uk'] = 'U.K.';
-oGeditions['au'] = 'Australia';
-oGeditions['nl_be'] = 'België';
-oGeditions['fr_be'] = 'Belgique';
-oGeditions['ca'] = 'Canada English';
-oGeditions['fr_ca'] = 'Canada Français';
-oGeditions['cs_cz'] = 'Česká republika';
-oGeditions['de'] = 'Deutschland';
-oGeditions['es'] = 'España';
-oGeditions['el_gr'] = 'Ελλάδα';
-oGeditions['fr'] = 'France';
-oGeditions['in'] = 'India';
-oGeditions['en_ie'] = 'Ireland';
-oGeditions['en_il'] = 'Israel';
-oGeditions['it'] = 'Italia';
-oGeditions['hu_hu'] = 'Magyarország';
-oGeditions['nl_nl'] = 'Nederland';
-oGeditions['nz'] = 'New Zealand';
-oGeditions['no_no'] = 'Norge';
-oGeditions['de_at'] = 'Österreich';
-oGeditions['pl_pl'] = 'Polska';
-oGeditions['pt-PT_pt'] = 'Portugal';
-oGeditions['ru_ru'] = 'Россия';
-oGeditions['de_ch'] = 'Schweiz';
-oGeditions['fr_ch'] = 'Suisse';
-oGeditions['en_za'] = 'South Africa';
-oGeditions['sv_se'] = 'Sverige';
-oGeditions['tr_tr'] = 'Türkiye';
-oGeditions['ru_ua'] = 'Украина';
-oGeditions['uk_ua'] = 'Україна';
-oGeditions['ar_me'] = 'العالم العربي'; /*(Arab world)*/
-oGeditions['ar_ae'] = 'الإمارات'; /*(UAE)*/
-oGeditions['ar_lb'] = 'لبنان'; /*(Lebanon)*/
-oGeditions['ar_sa'] = 'السعودية';/* (KSA)*/
-oGeditions['ar_eg'] = 'مصر';/* (Egypt)*/
-oGeditions['iw_il'] = 'ישראל';/* (Israel)*/
-oGeditions['cn'] = '中国版';
-oGeditions['hk'] = '香港版';
-oGeditions['jp'] = '日本';
-oGeditions['kr'] = '한국';
-oGeditions['tw'] = '台灣版';
-oGeditions['hi_in'] = 'भारत';
-oGeditions['en_my'] = 'Malaysia';
-oGeditions['en_pk'] = 'Pakistan';
-oGeditions['en_ph'] = 'Philippines';
-oGeditions['en_sg'] = 'Singapore';
-oGeditions['vi_vn'] = 'Việt Nam';
-oGeditions['es_ar'] = 'Argentina';
-oGeditions['pt-BR_br'] = 'Brasil';
-oGeditions['es_cl'] = 'Chile';
-oGeditions['es_co'] = 'Colombia';
-oGeditions['es_cu'] = 'Cuba';
-oGeditions['es_us'] = 'Estados Unidos';
-oGeditions['es_mx'] = 'México';
-oGeditions['es_pe'] = 'Perú';
-oGeditions['es_ve'] = 'Venezuela';
-oGeditions['en_bw'] = 'Botswana';
-oGeditions['en_et'] = 'Ethiopia';
-oGeditions['en_gh'] = 'Ghana';
-oGeditions['en_ke'] = 'Kenya';
-oGeditions['en_na'] = 'Namibia';
-oGeditions['en_ng'] = 'Nigeria';
-oGeditions['fr_sn'] = 'Sénégal';
-oGeditions['en_tz'] = 'Tanzania';
-oGeditions['en_ug'] = 'Uganda';
-oGeditions['en_zw'] = 'Zimbabwe';
-
-var oWpedia = Object();
-oWpedia['ar'] = 'العربية';
-oWpedia['ca'] = 'Català';
-oWpedia['cs'] = 'Česky';
-oWpedia['da'] = 'Dansk';
-oWpedia['de'] = 'Deutsch';
-oWpedia['en'] = 'English';
-oWpedia['es'] = 'Español';
-oWpedia['eo'] = 'Esperanto';
-oWpedia['fr'] = 'Français';
-oWpedia['ko'] = '한국어';
-oWpedia['id'] = 'Bahasa Indonesia';
-oWpedia['it'] = 'Italiano';
-oWpedia['he'] = 'עברית';
-oWpedia['lt'] = 'Lietuvių';
-oWpedia['hu'] = 'Magyar';
-oWpedia['nl'] = 'Nederlands';
-oWpedia['ja'] = '日本語';
-oWpedia['no'] = 'Norsk (bokmål)';
-oWpedia['pl'] = 'Polski';
-oWpedia['pt'] = 'Português';
-oWpedia['ru'] = 'Русский';
-oWpedia['ro'] = 'Română';
-oWpedia['sk'] = 'Slovenčina';
-oWpedia['sr'] = 'Српски';
-oWpedia['fi'] = 'Suomi';
-oWpedia['sv'] = 'Svenska';
-oWpedia['tr'] = 'Türkçe';
-oWpedia['uk'] = 'Українська';
-oWpedia['vi'] = 'Tiếng Việt';
-oWpedia['vo'] = 'Volapük';
-oWpedia['zh'] = '中文';
-
-var oImdb = Object();
-oImdb['en'] = 'English';
-oImdb['de'] = 'Deutsch';
-oImdb['fr'] = 'Français';
-oImdb['es'] = 'Español';
-oImdb['it'] = 'Italiano';
-oImdb['pt'] = 'Português';
-
-var oAmazon = Object();
-oAmazon['com'] = 'US';
-oAmazon['co.uk'] = 'UK';
-oAmazon['ca'] = 'Canada';
-oAmazon['cn'] = 'China';
-oAmazon['fr'] = 'France';
-oAmazon['de'] = 'Germany';
-oAmazon['co.jp'] = 'Japan';
-
-var oMusic = Object();
-oMusic['artist'] = 'Artist';
-oMusic['release'] = 'Release';
-oMusic['track'] = 'Track';
-
-var oMaemo = Object();
-oMaemo['talk'] = 'Talk';
-oMaemo['packages'] = 'Packages';
-oMaemo['wiki'] = 'Wiki';
-
+var oLangs = {
+'dk':'Dansk','nl':'Nederlands','en':'English','fi':'Suomi','fr':'Français','de':'Deutsch',
+'it':'Italiano','no':'Norsk','ro':'Română','ru':'Русский','es':'Español','tr':'Türkçe'}
+var oGtlate = {'af':'Afrikaans','sq':'Albanian','ar':'Arabic','be':'Belarusian',
+'bg':'Bulgarian','ca':'Catalan','zh-CN':'Chinese (Simplified)',
+'zh-TW':'Chinese (Traditional)','hr':'Croatian','cs':'Czech','da':'Danish',
+'nl':'Dutch','en':'English','et':'Estonian','tl':'Filipino','fi':'Finnish',
+'fr':'French','gl':'Galician','de':'German','el':'Greek','ht':'Haitian Creole',
+'iw':'Hebrew','hi':'Hindi','hu':'Hungarian','is':'Icelandic','id':'Indonesian',
+'ga':'Irish','it':'Italian','ja':'Japanese','lv':'Latvian','lt':'Lithuanian',
+'mk':'Macedonian','ko':'Korean','ms':'Malay','mt':'Maltese','no':'Norwegian',
+'fa':'Persian','pl':'Polish','pt':'Portuguese','ro':'Romanian','ru':'Russian',
+'sr':'Serbian','sk':'Slovak','sl':'Slovenian','es':'Spanish','sw':'Swahili',
+'sv':'Swedish','th':'Thai','tr':'Turkish','uk':'Ukrainian','vi':'Vietnamese',
+'cy':'Welsh','yi':'Yiddish'}
+var oGlangs = {'lang_af':'Afrikaans',
+'lang_ar':'Arabic','lang_be':'Belarusian','lang_bg':'Bulgarian','lang_ca':'Catalan',
+'lang_zh-CN':'Chinese (Simplified)','lang_zh-TW':'Chinese (Traditional)','lang_hr':'Croatian',
+'lang_cs':'Czech','lang_da':'Danish','lang_nl':'Dutch','lang_en':'English',
+'lang_eo':'Esperanto','lang_et':'Estonian','lang_tl':'Filipino','lang_fi':'Finnish',
+'lang_fr':'French','lang_de':'German','lang_el':'Greek','lang_iw':'Hebrew',
+'lang_hu':'Hungarian','lang_is':'Icelandic','lang_id':'Indonesian','lang_it':'Italian',
+'lang_ja':'Japanese','lang_ko':'Korean','lang_lv':'Latvian','lang_lt':'Lithuanian',
+'lang_no':'Norwegian','lang_fa':'Persian','lang_pl':'Polish','lang_pt':'Portuguese',
+'lang_ro':'Romanian','lang_ru':'Russian','lang_sr':'Serbian','lang_sk':'Slovak',
+'lang_sl':'Slovenian','lang_es':'Spanish','lang_sw':'Swahili','lang_sv':'Swedish',
+'lang_th':'Thai','lang_tr':'Turkish','lang_uk':'Ukrainian','lang_vi':'Vietnamese'}
+var oGeditions = {'us':'U.S.','uk':'U.K.','au':'Australia','nl_be':'België',
+'fr_be':'Belgique','ca':'Canada English','fr_ca':'Canada Français','cs_cz':'Česká republika',
+'de':'Deutschland','es':'España','el_gr':'Ελλάδα','fr':'France','in':'India',
+'en_ie':'Ireland','en_il':'Israel','it':'Italia','hu_hu':'Magyarország',
+'nl_nl':'Nederland','nz':'New Zealand','no_no':'Norge','de_at':'Österreich',
+'pl_pl':'Polska','pt-PT_pt':'Portugal','ru_ru':'Россия','de_ch':'Schweiz',
+'fr_ch':'Suisse','en_za':'South Africa','sv_se':'Sverige','tr_tr':'Türkiye',
+'ru_ua':'Украина','uk_ua':'Україна','ar_me':'العالم العربي', /*(Arab world)*/
+'ar_ae':'الإمارات', /*(UAE)*/'ar_lb':'لبنان', /*(Lebanon)*/
+'ar_sa':'السعودية',/* (KSA)*/'ar_eg':'مصر',/* (Egypt)*/'iw_il':'ישראל',/* (Israel)*/
+'cn':'中国版','hk':'香港版','jp':'日本','kr':'한국','tw':'台灣版','hi_in':'भारत',
+'en_my':'Malaysia','en_pk':'Pakistan','en_ph':'Philippines','en_sg':'Singapore',
+'vi_vn':'Việt Nam','es_ar':'Argentina','pt-BR_br':'Brasil','es_cl':'Chile',
+'es_co':'Colombia','es_cu':'Cuba','es_us':'Estados Unidos','es_mx':'México',
+'es_pe':'Perú','es_ve':'Venezuela','en_bw':'Botswana','en_et':'Ethiopia',
+'en_gh':'Ghana','en_ke':'Kenya','en_na':'Namibia','en_ng':'Nigeria',
+'fr_sn':'Sénégal','en_tz':'Tanzania','en_ug':'Uganda','en_zw':'Zimbabwe'}
+var oWpedia = {'ar':'العربية','ca':'Català',
+'cs':'Česky','da':'Dansk','de':'Deutsch','en':'English','es':'Español',
+'eo':'Esperanto','fr':'Français','ko':'한국어','id':'Bahasa Indonesia',
+'it':'Italiano','he':'עברית','lt':'Lietuvių','hu':'Magyar','nl':'Nederlands',
+'ja':'日本語','no':'Norsk (bokmål)','pl':'Polski','pt':'Português','ru':'Русский',
+'ro':'Română','sk':'Slovenčina','sr':'Српски','fi':'Suomi','sv':'Svenska',
+'tr':'Türkçe','uk':'Українська','vi':'Tiếng Việt','vo':'Volapük','zh':'中文'}
+var oImdb = {'en':'English','de':'Deutsch','fr':'Français',
+'es':'Español','it':'Italiano','pt':'Português'}
+var oAmazon = {'com':'US','co.uk':'UK','ca':'Canada',
+'cn':'China','fr':'France','de':'Germany','co.jp':'Japan'}
+var oMusic = {'artist':'Artist','release':'Release','track':'Track'}
+var oMaemo = {'talk':'Talk','packages':'Packages','wiki':'Wiki',}
 
 var ServiceOptions = function() {
     var imdb = oImdb;
